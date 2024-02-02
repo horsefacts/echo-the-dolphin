5ba0ef0968e8e7b4676b558e26cd629d3e16c6ab
horsefacts <webmaster@terminally.online>
2024-02-02 16:21:45 -0500

    typo fix

--- src/app/components/Echo.tsx
+++ src/app/components/Echo.tsx
@@ -1,7 +1,7 @@
 export default function Echo() {
   return (
     <div className="space-y-4">
-      <h1 className="text-8xl font-bold">🐬 Echo the Dolpin</h1>
+      <h1 className="text-8xl font-bold">🐬 Echo the Dolphin</h1>
       <a className="text-4xl text-sky-500 underline" href="https://github.com/horsefacts/echo-the-dolphin" target="_blank">See the code on Github</a>
     </div>
   );
